0778f2a37ca460e2a8e36065fcaf617a99af0edb
Harek_k <m171165@gmail.com>
2020-05-23 18:19:29 +0300

    123

--- ats_703/main_script.py
+++ ats_703/main_script.py
@@ -112,19 +112,54 @@ for loc_i in range(len(students)):
 #===================================================================================
 #Якушенков Константин
 
+result = []
+result.append('Апидова_Анастас\n')
+result.append('406\n')
+result.append('1_1\n')
+result.append('Fail\n')
+result.append('1 - Fail\n2 - Fail\n3 - Pass\n4 - Pass\n5 - Fail\n')
+
 def Preload():
     worksheet["A1"] =  'Студент'
     worksheet["B1"] =  'Группа'
-    worksheet["C1"] =  'Задание'
-    worksheet["D1"] =  'Результат тестирования'
+    worksheet["C1"] =  '1.1'
+    worksheet["D1"] =  '1.2'
+    worksheet["E1"] =  '1.3'
+    worksheet["F1"] =  '1.4'
+    worksheet["G1"] =  '1.5'
+    worksheet["H1"] =  '2.1'
+    worksheet["I1"] =  '2.2'
+    worksheet["J1"] =  '2.3'
+    worksheet["K1"] =  '2.4'
+    worksheet["L1"] =  '2.5'
+    worksheet["M1"] =  '3.1'
+    worksheet["N1"] =  '3.2'
+    worksheet["O1"] =  '3.3'
+    worksheet["P1"] =  '3.4'
+    worksheet["Q1"] =  '3.5'
+    worksheet["R1"] =  '4.6'
+    worksheet["S1"] =  '4.7'
+    worksheet["T1"] =  '4.8'
+    worksheet["U1"] =  '4.9'
+    worksheet["V1"] =  '5.4'
+    worksheet["W1"] =  '5.5'
+    worksheet["X1"] =  '5.6'
+    worksheet["Y1"] =  '5.7'
+    worksheet["Z1"] =  '5.8'
+    worksheet["AA1"] =  '5.9'
+    worksheet["AB1"] =  '5.10'
+    worksheet["AC1"] =  '6.1'
+    worksheet["AD1"] =  '6.2'
 
 #расстояние Левенштейна
 def Levenshtein(comparedString1, comparedString2):
     n, m = len(comparedString1), len(comparedString2)
+
     if n > m:
         comparedString1, comparedString2 = comparedString2, comparedString1
         n, m = m, n
     current_row = range(n + 1)
+
     for i in range(1, m + 1):
         previous_row, current_row = current_row, [i] + [0] * n
         for j in range(1, n + 1):
@@ -132,6 +167,7 @@ def Levenshtein(comparedString1, comparedString2):
             if comparedString1[j - 1] != comparedString2[i - 1]:
                 change += 1
             current_row[j] = min(add, delete, change)
+
     return current_row[n]
 
 #применение этого расстояния
@@ -139,28 +175,34 @@ def CompareStrings(name_counter):
     i = 0
     comparedName = ''
     comparedSurname = ''
+
     while result[0][i] != '_':
         comparedSurname = comparedSurname + result[0][i]
         i = i + 1
     i = i + 1
+
     while result[0][i] != '\n':
         comparedName = comparedName + result[0][i]
         i = i + 1
     i = 0
     perfectName = ''
     perfectSurname = ''
+
     while studentsList[name_counter][i] != '_':
         perfectSurname = perfectSurname + studentsList[name_counter][i]
         i = i + 1
     i = i + 1
+
     while studentsList[name_counter][i] != '\n':
         perfectName = perfectName + studentsList[name_counter][i]
         i = i + 1
+
     i = 0
     dist1 = Levenshtein(comparedName,perfectName)
     dist2 = Levenshtein(comparedSurname, perfectSurname)
     dist3 = Levenshtein(comparedName, perfectSurname)
     dist4 = Levenshtein(comparedSurname, perfectName)
+
     if ((global_a[0] != '9') and (global_a[0] != '1')):
         if((dist1 != 0) and (dist2 != 0)):
             if(dist1 < dist3):
@@ -185,6 +227,7 @@ def CompareStrings(name_counter):
                         global_a[1] = name_counter + 2
     else:
         pass
+
     if (studentsList[name_counter] != 'Якушенков_Константин\n'):
         name_counter = name_counter + 1
         CompareStrings(name_counter)
@@ -197,14 +240,202 @@ def WriterXLSX():
     temp_cell = 'B'
     temp_cell = temp_cell + str(global_a[1])
     worksheet[temp_cell] =  result[1]
-    temp_cell = 'C'
-    temp_cell = temp_cell + str(global_a[1])
-    worksheet[temp_cell] =  result[2]
-    temp_cell = 'D'
-    temp_cell = temp_cell + str(global_a[1])
-    worksheet[temp_cell] =  result[3]
-    comment = Comment(result[4], "Results")
-    worksheet[temp_cell].comment = comment
+
+    if result[2] == '1_1':
+        temp_cell = 'C'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '1_2':
+        temp_cell = 'D'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '1_3':
+        temp_cell = 'E'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '1_4':
+        temp_cell = 'F'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '1_5':
+        temp_cell = 'G'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '2_1':
+        temp_cell = 'H'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '2_2':
+        temp_cell = 'I'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '2_3':
+        temp_cell = 'J'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '2_4':
+        temp_cell = 'K'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '2_5':
+        temp_cell = 'L'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '3_1':
+        temp_cell = 'M'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '3_2':
+        temp_cell = 'N'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '3_3':
+        temp_cell = 'O'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '3_4':
+        temp_cell = 'P'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '3_5':
+        temp_cell = 'Q'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '4_6':
+        temp_cell = 'R'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '4_7':
+        temp_cell = 'S'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '4_8':
+        temp_cell = 'T'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '4_9':
+        temp_cell = 'U'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_4':
+        temp_cell = 'V'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_5':
+        temp_cell = 'W'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_6':
+        temp_cell = 'X'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+        
+    if result[2] == '5_7':
+        temp_cell = 'Y'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_8':
+        temp_cell = 'Z'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_9':
+        temp_cell = 'AA'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '5_10':
+        temp_cell = 'AB'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '6_1':
+        temp_cell = 'AC'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
+
+    if result[2] == '6_2':
+        temp_cell = 'AD'
+        temp_cell = temp_cell + str(global_a[1])
+        worksheet[temp_cell] =  result[3]
+        comment = Comment(result[4], "Results")
+        worksheet[temp_cell].comment = comment
 
 
 
@@ -243,13 +474,6 @@ studentsList.append('Якушенков_Константин\n')
 
 Preload()
 
-result = []
-result.append('Апидова_Анастас\n')
-result.append('406\n')
-result.append('1_1\n')
-result.append('Fail\n')
-result.append('1 - Fail\n2 - Fail\n3 - Pass\n4 - Pass\n5 - Fail\n')
-
 name_counter = 0
 
 global_a = []
@@ -257,6 +481,7 @@ global_a.append(5)
 global_a.append(0)
 
 CompareStrings(name_counter) 
+
 if(global_a[1] != 0):   
     WriterXLSX()
 
